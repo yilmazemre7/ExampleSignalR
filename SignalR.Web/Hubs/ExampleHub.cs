6004a10c418b5b279709b8a6c77c7034565b7439
Emre Yılmaz <yilmazemrecu@gmail.com>
2026-03-14 15:03:23 +0300

    Add project files.

---
+++ SignalR.Web/Hubs/ExampleHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Web.Hubs
+{
+    public class ExampleHub : Hub
+    {
+        public async Task BroadcastMessageToAllClient(string message)
+        {
+            await Clients.All.SendAsync(message);
+        }
+    }
+}
